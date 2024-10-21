eadec16832ae3e2f582911f4c63c42b7faa6fa15
imperfectsworld <imperfectsworld@gmail.com>
2024-10-21 15:43:38 -0400

    Changed the url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiDomain: "https://green-sand-0fc2ba30f.5.azurestaticapps.net", //replace this with your deployed backend API's url
+    apiDomain: "https://karmabackend.azurewebsites.net", //replace this with your deployed backend API's url
     apiKey:""
 };
