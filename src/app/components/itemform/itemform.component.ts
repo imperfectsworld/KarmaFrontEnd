32f2d6c11dcb0bbf00e5c5b0e4e78ea63e8d64e9
kingDVD <davidvgoodwin@gmail.com>
2024-10-22 14:48:44 -0400

    styling p2

---
+++ src/app/components/itemform/itemform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itemform',
+  standalone: true,
+  imports: [],
+  templateUrl: './itemform.component.html',
+  styleUrl: './itemform.component.css'
+})
+export class ItemformComponent {
+
+}
