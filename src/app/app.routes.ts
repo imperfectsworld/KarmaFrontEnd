a4fa001a1ef1acff20caddf5773618c596f097b3
imperfectsworld <imperfectsworld@gmail.com>
2024-10-23 10:17:12 -0400

    Added assets folder and started design on the homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { ChatComponent } from './components/chat/chat.component';
 import { UserComponent } from './components/user/user.component';
 import { JoinRoomComponent } from './components/join-room/join-room.component';
 import { ItemComponent } from './components/item/item.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path:"user",component:UserComponent},
     {path:"chat",component:ChatComponent},
     {path:"join-room",component:JoinRoomComponent},
     {path:"item",component:ItemComponent},
-    {path:"",redirectTo:"/user",pathMatch:"full"},
-    { path: "**", redirectTo: "/user" }
+    {path:"home",component:HomeComponent},
+    {path:"",redirectTo:"/home",pathMatch:"full"},
+    {path: "**", redirectTo: "/home"}
 ];
