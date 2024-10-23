4c4b3803ab3de8d246aab85e15c8e4d0b7461f74
Afseen Salam <afseenfazil@gmail.com>
2024-10-23 15:59:19 -0400

    Added new navbar in all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { GoogleSigninButtonModule, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { JoinRoomComponent } from './components/join-room/join-room.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,66 @@
+import { SocialUser, SocialAuthService, GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { User } from '../../models/user';
+import { BackendService } from '../../services/backend.service';
+import { FormsModule } from '@angular/forms';
+import { JoinRoomComponent } from '../join-room/join-room.component';
+import { ItemComponent } from '../item/item.component';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule,JoinRoomComponent,GoogleSigninButtonModule,ItemComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  googleUser: SocialUser = {} as SocialUser;
+  loggedIn: boolean = false;
+  user: User = {} as User;
+  
+  @Output() userLoggedIn: EventEmitter<User> = new EventEmitter<User>();
+  constructor(private activateRoute:ActivatedRoute,private backendService: BackendService, private socialAuthServiceConfig: SocialAuthService, private router: Router){}
+
+  ngOnInit(){
+    this.socialAuthServiceConfig.authState.subscribe((userResponse: SocialUser) => {
+      this.googleUser = userResponse;
+      this.loggedIn = (userResponse != null);
+      if(this.loggedIn){
+        this.user = {
+          googleId: this.googleUser.id,
+          userName: this.googleUser.name,
+          profilePic: this.googleUser.photoUrl
+        };
+        this.backendService.addUser(this.user).subscribe(response => {console.log(response)});
+      }
+    })
+  }
+  // loginWithGoogle(): void {
+  //   this.socialAuthServiceConfig.signIn('google').then(user => {
+  //     console.log("User logged in:", user);
+  //   }).catch(error => {
+  //     console.error("Login failed:", error);
+  //   });
+  // }
+  signOut(): void{
+    this.socialAuthServiceConfig.signOut()
+    location.reload();
+    this.user = {} as User;
+  }
+  navigateToJoinRoom() {
+    this.router.navigate(['/join-room']);
+  }
+  closeNavbar() {
+    const navbar = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.getElementById('navbarNav');
+    
+    if (navbar && navbarCollapse && navbarCollapse.classList.contains('show')) {
+      (navbar as HTMLElement).click();  // Trigger the toggle
+    }
+  }
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
+
+}
